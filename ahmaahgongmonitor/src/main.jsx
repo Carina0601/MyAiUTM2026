cb3b3a053df614d3100b4dccf45fd969451f0c18
Leong Jia Ni <carinaleong1606@gmail.com>
2026-04-12 17:38:16 +0800

    Delete ahmaahgongmonitor directory

--- ahmaahgongmonitor/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import MonitorRun from './MonitorRun.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <MonitorRun />
-  </StrictMode>,
-)
