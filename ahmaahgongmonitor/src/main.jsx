1db76a44dd9bef3beeb2cca18b0946bf12b312d8
Engg06 <loyky.hongxuan@gmail.com>
2026-04-18 21:27:24 +0800

    .

--- ahmaahgongmonitor/src/main.jsx
+++ ahmaahgongmonitor/src/main.jsx
@@ -5,6 +5,7 @@ import Navbar from './Navbar.jsx'
 import MonitorRun from './MonitorRun.jsx'
 import SpeechPage from './SpeechPage.jsx'
 import SummaryPage from './SummaryPage.jsx'
+import AmbulanceMonitor from './AmbulanceMonitor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,8 +13,9 @@ createRoot(document.getElementById('root')).render(
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<MonitorRun></MonitorRun>}></Route>
-        <Route path="/speech" element={<SpeechPage></SpeechPage>}></Route>
-        <Route path="/summary" element={<SummaryPage></SummaryPage>}></Route>
+        <Route path="/ambulance" element={<AmbulanceMonitor></AmbulanceMonitor>}></Route>
+        <Route path="/speech/:id" element={<SpeechPage></SpeechPage>}></Route>
+        <Route path="/summary/:id" element={<SummaryPage></SummaryPage>}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
